f9e4a2d547b00cb5623c802822c2d5149df99686
Alec Caro <alec.caro@gmail.com>
2021-01-22 08:58:10 -0500

    O

---
+++ w4d5/octopus/octo.rb
@@ -0,0 +1,14 @@
+fishes = ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh']
+
+def sluggish(arr)
+  longest = arr[0]
+  (0...arr.length - 1).each do |i|
+    (i+1...arr.length).each do |j|
+      
+    end
+  end
+end
+
+def dom
+
+end
