13b6bc8ab685b012b4a1e76645267635efa6188f
Alec Caro <alec.caro@gmail.com>
2021-02-18 09:42:34 -0500

    mornin

--- w8d3/tonights_special.js
+++ w8d3/tonights_special.js
@@ -11,7 +11,8 @@
 // Make sure it works before moving on!
 
 function titleize(arr, callback) {
-  let titleized = arr.map(ele => `Mx.${ele} Jingleheimer Schmidt`);
+  let titleized = arr.map(function (ele) {
+    return `Mx.${ele} Jingleheimer Schmidt`});
   callback(titleized);
 }
 
@@ -24,10 +25,10 @@ function printCallback(arr) {
 titleize(["Mary", "Brian", "Leo"], printCallback)
 
 
-function Elephant(name, height, tricks) {
+function Elephant(name, height, ...tricks) {
   this.name = name;
   this.height = height;
-
+  this.tricks = tricks;
 }
 
 Elephant.prototype.trumpet = function(){
@@ -40,7 +41,35 @@ Elephant.prototype.grow = function(){
 
 let hammad = new Elephant("Hammad", 72);
 
+Elephant.prototype.addTrick = function(trick) {
+  this.tricks.push(trick)
+
+}
+  
+Elephant.prototype.play = function() {
+  let rand = Math.floor(Math.random()*this.tricks.length);
+  console.log(`${this.name} is ${this.tricks[rand]}`)
+}
+
 console.log(hammad)
 hammad.trumpet()
 hammad.grow()
 console.log(hammad)
+
+let ellie = new Elephant("Ellie", 185, ["giving human friends a ride", "playing hide and seek"]);
+
+let charlie = new Elephant("Charlie", 200, ["painting pictures", "spraying water for a slip and slide"]);
+
+let kate = new Elephant("Kate", 234, ["writing letters", "stealing peanuts"]);
+
+let micah = new Elephant("Micah", 143, ["trotting", "playing tic tac toe", "doing elephant ballet"]);
+
+let herd = [hammad, ellie, charlie, kate, micah];
+
+Elephant.paradeHelper =  function(elephant) {
+  console.log(`${elephant.name} is trotting by!`)
+}
+
+// herd.forEach(function (elephant) {Elephant.paradeHelper(elephant)})
+herd.forEach(Elephant.paradeHelper)
+
